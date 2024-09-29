e5b74c29d2dded7630ac6193a142560111145d09
Ahmad <ahmad.waeldemerdash@gmail.com>
2024-09-29 22:38:39 +0300

    final

--- Builder/static/Builder/cv.js
+++ Builder/static/Builder/cv.js
@@ -1,40 +1,41 @@
 document.addEventListener('DOMContentLoaded', function () {
-   const buttons= document.querySelectorAll('#edit_senior, #edit_junior')
+   const buttons= document.querySelectorAll('.edit')
+   console.log(buttons);
     
    buttons.forEach(function(button){
     button.onclick = function(){
         user_input = document.querySelectorAll('.change');
         user_input.forEach(function(input){
             const text_area = document.createElement('input');
-            const save = document.querySelector('#bar');
+            const save = document.querySelector('.save');
             save.style.display = 'block';
             input.onclick = (event) =>{
                 const foo = event.target
                 event.target.innerHTML = '' 
                 event.target.appendChild(text_area);
+                save.click()
                 save.onclick = function(){
                     foo.innerHTML = ''
                     foo.innerHTML = text_area.value
-                    save.style.display = 'none';
                     const download = document.querySelector('.download')
                     download.style.display = 'block';
-                    download.onclick = function(){
+                download.onclick = function(){
                     download.style.display = 'none';
                     save.style.display = 'none';
-                    document.querySelector('#edit_junior').style.display = 'none';
+                    document.querySelector('.edit').style.display = 'none';
                     div = document.querySelector('.padding')
-                    html2pdf()
-                    .from(div)
-                    .set({
-                        html2canvas: {scale: 2},
-                        margin: 1,
-                        jsPDF: {orientation: "portrait"},
-                        filename: 'CV.pdf',
-                    })     
-                    .save()    
-                    }   
+                html2pdf()
+                .from(div)
+                .set({
+                    html2canvas: {scale: 2},
+                    margin: 1,
+                    jsPDF: {orientation: "portrait"},
+                    filename: 'CV.pdf',
+                        })     
+                        .save()    
+                        }   
+                    }
                 }
-            }
         })
     }
    })
